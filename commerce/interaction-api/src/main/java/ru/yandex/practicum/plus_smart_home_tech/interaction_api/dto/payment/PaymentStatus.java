f52d308119d4a71268fc50fbd496c1b1001f5677
IT-Egor <egor.belousov.04@yandex.ru>
2025-07-23 19:16:22 +0300

    add payment model

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/plus_smart_home_tech/interaction_api/dto/payment/PaymentStatus.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.plus_smart_home_tech.interaction_api.dto.payment;
+
+public enum PaymentStatus {
+    PENDING,
+    SUCCESS,
+    FAILED
+}
---
+++ commerce/payment/src/main/java/ru/yandex/practicum/plus_smart_home_tech/payment/model/Payment.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.plus_smart_home_tech.payment.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import ru.yandex.practicum.plus_smart_home_tech.interaction_api.dto.payment.PaymentStatus;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "payment_id")
+    private UUID paymentId;
+
+    @Column(name = "order_id")
+    private UUID orderId;
+
+    @Column(name = "total_payment")
+    private Double totalPayment;
+
+    @Column(name = "delivery_total")
+    private Double deliveryTotal;
+
+    @Column(name = "fee_total")
+    private Double feeTotal;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status")
+    private PaymentStatus paymentStatus;
+}
